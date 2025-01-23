075406b5ae0f55063e07341e360dc44263708891
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-24 04:02:14 +0900

    fix: lint 구조 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,62 +9,57 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  "airbnb",
-  "airbnb-typescript",
-  "eslint:recommended",
-  "plugin:@typescript-eslint/recommended",
-  "plugin:react/recommended",
-  "plugin:prettier/recommended",
-  "plugin:storybook/recommended",
-  "plugin:@tanstack/eslint-plugin-query/recommended",
-];
-
-const rules = {
-  "react/react-in-jsx-scope": "off",
-  "react/jsx-filename-extension": [
-    "warn",
-    {
-      extensions: [".ts", ".tsx", ".jsx", ".js"],
-    },
-  ],
-  "no-plusplus": "off",
-  "react/function-component-definition": "off",
-  "@typescript-eslint/camelcase": "off",
-  "@typescript-eslint/no-explicit-any": "off",
-  "prettier/prettier": [
-    "error",
-    {
-      endOfLine: "auto",
-    },
-  ],
-  "import/extensions": [
-    "error",
-    "ignorePackages",
-    {
-      ts: "never",
-      tsx: "never",
-    },
-  ],
-  "import/prefer-default-export": "off",
-  "no-console": ["error", { allow: ["warn", "error"] }],
-  "import/no-extraneous-dependencies": [
-    "error",
-    {
-      devDependencies: true,
-    },
-  ],
-  "jsx-a11y/label-has-associated-control": [
-    "error",
-    {
-      assert: "htmlFor",
-    },
-  ],
-};
-
 export default {
-  ...eslintConfig,
-  rules,
+  extends: [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    "airbnb",
+    "airbnb-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".ts", ".tsx", ".jsx", ".js"],
+      },
+    ],
+    "no-plusplus": "off",
+    "react/function-component-definition": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+      },
+    ],
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        assert: "htmlFor",
+      },
+    ],
+  },
 };
