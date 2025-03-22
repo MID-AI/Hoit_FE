dc63fa94e8c951c4d06c947797cb23a9aacc4689
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-03-22 12:53:44 +0900

    fix: 구글 로그인 설정 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,5 @@
-import Link from "next/link";
-
 function Test() {
-  return (
-    <div>
-      <h2>구글 로그인 테스트</h2>
-      <Link href="/oauth2/authorization/google">구글 로그인</Link>
-    </div>
-  );
+  return <div>test</div>;
 }
 
 export default Test;
