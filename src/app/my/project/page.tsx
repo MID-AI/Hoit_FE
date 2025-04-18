921bb5a57d5365dfe7168e8891e46b3b9e6d9707
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-04-19 03:48:31 +0900

    feat: 탭 구분 쿼리스트링 추가

--- src/app/my/project/page.tsx
+++ src/app/my/project/page.tsx
@@ -1,31 +1,10 @@
-import { getMyImageList } from "@/apis/services/project";
-
-import InfinitePrefetch from "@/components/query/infinite-prefetch";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import TabAll from "@/components/user/project/tab-all";
-import TabFolder from "@/components/user/project/tab-folder";
-import { QUERY_KEY } from "@/constants/query-key";
+import TabAllWrapper from "@/components/user/project/tab/tab-all-wrapper";
+import TabWrapper from "@/components/user/project/tab/tab-wrapper";
 
 function MyProject() {
   return (
     <div className="w-full px-23 py-64">
-      <Tabs defaultValue="all" className="w-full">
-        <TabsList className="h-44">
-          <TabsTrigger value="all">전체</TabsTrigger>
-          <TabsTrigger value="folder">폴더</TabsTrigger>
-        </TabsList>
-        <TabsContent value="all">
-          <InfinitePrefetch
-            queryKey={QUERY_KEY.MY.PROJECT()}
-            queryFn={({ pageParam = 0 }) => getMyImageList(pageParam)}
-          >
-            <TabAll />
-          </InfinitePrefetch>
-        </TabsContent>
-        <TabsContent value="folder">
-          <TabFolder />
-        </TabsContent>
-      </Tabs>
+      <TabWrapper tabAll={<TabAllWrapper />} />
     </div>
   );
 }
