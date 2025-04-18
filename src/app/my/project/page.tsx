7ef50a1a4c50dd9d9c51b3390f1de0b6a0e9316c
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-04-19 05:05:04 +0900

    fix: useSearchParams 에러 수정 - suspense 래핑 적용

--- src/app/my/project/page.tsx
+++ src/app/my/project/page.tsx
@@ -1,10 +1,13 @@
 import TabAllWrapper from "@/components/user/project/tab/tab-all-wrapper";
 import TabWrapper from "@/components/user/project/tab/tab-wrapper";
+import { Suspense } from "react";
 
 function MyProject() {
   return (
     <div className="w-full px-23 py-64">
-      <TabWrapper tabAll={<TabAllWrapper />} />
+      <Suspense fallback="로딩중(MyProject 컴포넌트)">
+        <TabWrapper tabAll={<TabAllWrapper />} />
+      </Suspense>
     </div>
   );
 }
