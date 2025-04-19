5bf34b3dcf57b078da0ff74fae79b3571a22e4e4
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-04-20 02:14:50 +0900

    feat: redirect용 로그인 페이지 생성, 빈 아이템 컴포넌트 생성, 로그인 uri 수정

---
+++ src/components/common/card/no-items.tsx
@@ -0,0 +1,18 @@
+import ScreenStarIcon from "@/assets/create/screen_star.svg";
+import cn from "@/utils/cn";
+
+function NoItems({ text, className }: { text: string; className?: string }) {
+  return (
+    <div
+      className={cn(
+        "flex h-full flex-col items-center justify-center gap-68 text-coolGray-400",
+        className,
+      )}
+    >
+      <ScreenStarIcon className="h-355 w-353 text-coolGray-200" />
+      <span className="text-Type-28-medium">{text}</span>
+    </div>
+  );
+}
+
+export default NoItems;
