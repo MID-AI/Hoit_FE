4448f83fc9476ac52f7bf6afc20e62ec3cb44b16
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-06-07 00:59:29 +0900

    fix: util 다운로드 함수 수정

--- src/utils/download.ts
+++
@@ -1,8 +0,0 @@
-export function downloadImage(url: string, filename: string) {
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = filename;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
